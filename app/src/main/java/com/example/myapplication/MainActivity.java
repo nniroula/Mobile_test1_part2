63be2665921503db8bc78c5357934e11b94d0866
nabin <nkbhaie2017@gmail.com>
2024-03-25 12:54:37 -0600

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // view binding variable
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
